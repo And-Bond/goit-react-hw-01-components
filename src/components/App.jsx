ef1e73289f96caefb9af7d02a118d26f9af4ab30
Andrey-Bondarevskij <aloxnesir@gmail.com>
2022-07-21 15:51:01 +0300

    second-half(Dont-work)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,22 @@
 import Profile from './profile/profile';
 import User from './user.json';
+import Statistics from './Statistics/statistics';
+import Data from './data.json';
 import PropTypes from 'prop-types';
 
 export const App = () => {
   return (
-    <Profile
-      username={User.username}
-      tag={User.tag}
-      location={User.location}
-      avatar={User.avatar}
-      followers={User.stats.followers}
-      views={User.stats.views}
-      likes={User.stats.likes}
-    />
+    <>
+      <Profile
+        username={User.username}
+        tag={User.tag}
+        location={User.location}
+        avatar={User.avatar}
+        followers={User.stats.followers}
+        views={User.stats.views}
+        likes={User.stats.likes}
+      />
+      <Statistics title="Upload" stats={Data} />
+    </>
   );
 };
